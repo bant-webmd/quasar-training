73b47a991f1c127582b360ba0a0a340633ee763c
Brandon Antonelli <bantonelli07@gmail.com>
2019-11-22 14:56:22 -0500

    Finish Section 7

---
+++ awesome-todo/src/components/Tasks/Task.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-item
+    clickable
+    v-ripple
+    @click="task.completed = !task.completed"
+    :class="{'bg-orange-1': !task.completed, 'bg-green-1': task.completed}"
+  >
+    <q-item-section side top>
+      <q-checkbox v-model="task.completed" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label :class="{'text-strikethrough': task.completed}">{{task.name}}</q-item-label>
+    </q-item-section>
+
+    <q-item-section side top>
+      <div class="row">
+        <div class="column justify-center">
+          <q-icon name="event" class="q-mr-md" />
+        </div>
+        <div class="column">
+          <q-item-label class="row justify-end" caption>{{task.dueDate}}</q-item-label>
+          <q-item-label class="row justify-end" caption>
+            <small>{{task.dueTime}}</small>
+          </q-item-label>
+        </div>
+      </div>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  props: ['id', 'task']
+};
+</script>
--- awesome-todo/src/pages/PageTodo.vue
+++ awesome-todo/src/pages/PageTodo.vue
@@ -2,35 +2,12 @@
   <q-page padding>
     <q-list separator bordered>
       <q-item-label header>General</q-item-label>
-
-      <q-item 
-        clickable
-        v-ripple
-        v-for="task in tasks"
-        :key="task.id"     
-        @click="task.completed = !task.completed"
-        :class="{'bg-orange-1': !task.completed, 'bg-green-1': task.completed}"   
-      >
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label :class="{'text-strikethrough': task.completed}">{{task.name}}</q-item-label>
-        </q-item-section>
-        
-        <q-item-section side top>
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon name="event" class="q-mr-md"/>
-            </div>
-            <div class="column">
-              <q-item-label class="row justify-end" caption>{{task.dueDate}}</q-item-label>     
-              <q-item-label class="row justify-end" caption><small>{{task.dueTime}}</small></q-item-label>        
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+      <task
+        v-for="(task, key) in tasks"
+        :key="key"
+        :id="key"
+        :task="task"
+      ></task>
     </q-list>
   </q-page>
 </template>
@@ -39,33 +16,20 @@
 </style>
 
 <script>
+import {mapGetters} from 'vuex';
+import Task from '../components/Tasks/Task.vue';
+
 export default {
-  data() {
-    return {
-      tasks: [
-        {
-          id: 1, 
-          name: "Go to shop",
-          completed: false,
-          dueDate: '2019/05/12',
-          dueTime: '18:30'
-        },
-        {
-          id: 2, 
-          name: "Get bananas",
-          completed: false, 
-          dueDate: '2019/05/13',
-          dueTime: '14:00'
-        },
-        {
-          id: 3, 
-          name: "Get apples",
-          completed: false, 
-          dueDate: '2019/05/14',
-          dueTime: '16:00'
-        }
-      ]
-    }
+  components: {
+    'task': Task
+  },
+  computed: {
+    // tasks() {
+    //   return this.$store.getters['tasks/tasks'];
+    // }
+    // Map getters from 'tasks' module 
+    // then specify which getters in the array
+    ...mapGetters('tasks', ['tasks'])
   }
 }
 </script>
